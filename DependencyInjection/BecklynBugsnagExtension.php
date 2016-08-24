50f82512648ef32017cfd77da65b6e5ed6881e10
Jannik Zschiesche <jz@becklyn.com>
2016-08-24 09:49:31 +0200

    Remove unused imports

--- DependencyInjection/BecklynBugsnagExtension.php
+++ DependencyInjection/BecklynBugsnagExtension.php
@@ -5,7 +5,6 @@ namespace Becklyn\BugsnagBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 
--- DependencyInjection/MonitoringCompilerPass.php
+++ DependencyInjection/MonitoringCompilerPass.php
@@ -9,7 +9,6 @@ use Monolog\Logger;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
 use Symfony\Component\DependencyInjection\Reference;
 
 
